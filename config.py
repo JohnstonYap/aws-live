c033faf7f47c1c3239f7a5426e07be827af5bc88
JohnstonYap <130417127+JohnstonYap@users.noreply.github.com>
2023-04-11 08:46:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cqhfmn45wiis.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "johnston-employee"
+custombucket = "johnston-employee"
+customregion = "us-east-1"
